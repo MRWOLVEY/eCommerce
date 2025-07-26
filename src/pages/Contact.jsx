f7c9a9d55854ca9fdaa269af49f7eb3619ec76bf
Mohammed Morra <wolfeyplays123@gmail.com>
2025-07-26 22:50:25 +0300

    tut done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import Title from '../components/Title'
+import NewsLetterBox from '../components/NewsLetterBox'
+import { assets } from '../assets/frontend_assets/assets'
+import { ShopContext } from '../contexts/ShopContext'
 
 const Contact = () => {
   return (
     <div>
-      
+      <div className="text-center text-2xl pt-10 border-t">
+        <Title t1={'contact'} t2={'us'} />
+      </div>
+      <div className="flex flex-col my-10 justify-center md:flex-row gap-10 mb-28">
+        <img className="w-full md:max-w-[480px]" src={assets.contact_img} alt="" />
+        <div className="flex flex-col justify-center ite gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">Address: 123 Street, New York, USA</p>
+          <p className="text-gray-500">
+            Phone: +1 234 567 890 <br /> Email: admin@store.com
+          </p>
+          <p className="text-gray-500 font-semibold text-xl">Careers</p>
+          <p className="text-gray-500">expolre our team and positions</p>
+          <button className="bg-black hover:opacity-85 text-white text-xs px-8 py-3 active:bg-gray-700 rounded-sm shadow-lg shadow-gray-500 transition-all duration-300">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+      <NewsLetterBox />
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [currentState, setCurrentState] = React.useState('Sign Up')
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+  }
   return (
     <div>
-      login
+      <form onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800" action="">
+        <div className="inline-flex items-center gap-2 mb-2 mt-10">
+          <p className="text-3xl prata-regular">{currentState}</p>
+          <hr className="w-8 border-none bg-gray-800 h-[1.5px]" />
+        </div>
+        {currentState === 'Sign Up' && <input type="text" className="w-full px-3 py-2 border border-gray-800" placeholder="Name" />}
+        <input type="email" className="w-full px-3 py-2 border border-gray-800" placeholder="Email" />
+        <input type="password" className="w-full px-3 py-2 border border-gray-800" placeholder="Password" />
+        <div className="w-full flex justify-between text-sm mt-[-8px]">
+          {currentState == 'Login' && (
+            <p className="cursor-pointer">
+              Forget your password? <img className="w-3 inline" src="/src/assets/megaMind.png" />
+            </p>
+          )}
+          {currentState === 'Sign Up' ? (
+            <p onClick={() => setCurrentState('Login')} className="cursor-pointer">
+              Already have an account?
+            </p>
+          ) : (
+            <p onClick={() => setCurrentState('Sign Up')} className="cursor-pointer">
+              Create an account
+            </p>
+          )}
+        </div>
+        <button className="bg-black hover:opacity-85 text-white text-xs px-8 py-3 active:bg-gray-700 rounded-sm shadow-lg shadow-gray-500">
+          {currentState == 'Login' ? 'Login' : 'Sign Up'}
+        </button>
+      </form>
     </div>
   )
 }
